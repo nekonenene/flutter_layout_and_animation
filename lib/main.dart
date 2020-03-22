d727a3221c128e7d00e38a43d04b4687e076ea7d
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-03-23 01:47:03 +0900

    titleSection Widget を設定

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,9 +15,42 @@ class MyApp extends StatelessWidget {
           title: Text('Layout Demo'),
         ),
         body: Center(
-          child: Text('Hello World!!'),
+          child: titleSection,
         ),
       )
     );
   }
+
+  Widget titleSection = Container(
+    padding: EdgeInsets.all(32),
+    child: Row(
+      children: [
+        // Expanded ウィジェット内に Column を置くことで、余りの領域すべてを使うことが出来る
+        Expanded(child: Column(
+          // 左側基準で始まることを宣言
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // padding を置きたいため Text 直接でなく、Container の内部に Text を置く作りにしている
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            // サブの文字列。グレーにしてある
+            Text(
+              'Kandersteg, Switzerland',
+              style: TextStyle(color: Colors.grey[500]),
+            )
+          ],
+        )),
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        Text('41'),
+      ],
+    ),
+  );
 }
